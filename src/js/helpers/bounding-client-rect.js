1953491fc1c72adef71046dd0833824a23ce8dc9
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2024-04-16 22:12:55 +0300

    add hero

--- src/js/helpers/bounding-client-rect.js
+++ src/js/helpers/bounding-client-rect.js
@@ -1,11 +1,13 @@
 function reportWindowSize() {
     const width = document.querySelector('html').getBoundingClientRect().width.toFixed(2);
     const height = document.querySelector('html').getBoundingClientRect().height.toFixed(2);
-    const heightHeader = document.querySelector('.search-popup .popup__content').getBoundingClientRect().height.toFixed(2);
+    const heightSearch = document.querySelector('.search-popup .popup__content').getBoundingClientRect().height.toFixed(2);
+    const heightHeader = document.querySelector('.header').getBoundingClientRect().height.toFixed(2);
     
     document.documentElement.style.setProperty('--vw', `${width}px`);
     document.documentElement.style.setProperty('--vh', `${height}px`);
-    document.documentElement.style.setProperty('--height-search', `${heightHeader}px`);
+    document.documentElement.style.setProperty('--height-search', `${heightSearch}px`);
+    document.documentElement.style.setProperty('--height-header', `${heightHeader}px`);
 }
 
 reportWindowSize();
